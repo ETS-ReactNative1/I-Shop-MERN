65fa29797162963af1b91770cefde134c1521707
Shay.Asanova <shay.asanova@gmail.com>
2021-11-16 15:08:38 +0400

    sign in form component

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -28,6 +28,7 @@ const CartScreen = ({match, location, history}) => {
   const checkoutHandler = () => {
     history.push('/login?redirect=shipping')
   }
+  
   return <Row>
       <Col md={8}>
        <h1>Shopping Cart</h1>
@@ -44,7 +45,7 @@ const CartScreen = ({match, location, history}) => {
                  </Col>
                  <Col md={2}> ${item.price}</Col>
                  <Col md={2}>
-                 <Form.Control as='select' value={item.qty} onChange={(e) => dispatch(addToCart(item.product,
+                 <Form.Control className='mt-1' as='select' value={item.qty} onChange={(e) => dispatch(addToCart(item.product,
                   Number(e.target.value)))}>
                      {[...Array(item.countInStock).keys()].map((x) => (
                          <option key={x+1} value={x+1}>
@@ -55,9 +56,11 @@ const CartScreen = ({match, location, history}) => {
                      </Form.Control>
                  </Col>
                  <Col>
+                 <div className='d-grid gap-2 mt-1'>
                    <Button type='button' variant='light' onClick={()=> removeFromCartHandler(item.product)}> 
                    <i className='fas fa-trash'></i>
                    </Button>
+                   </div>
                  </Col>
                </Row>
              </ListGroup.Item>
@@ -75,8 +78,8 @@ const CartScreen = ({match, location, history}) => {
                ${cartItems.reduce((acc,item)=> acc+ item.qty * item.price, 0).toFixed(2)}
             </ListGroup.Item>
             <ListGroup.Item>
-            <div className='d-grid gap-2'>
-              <Button type='button' className='btn-block' disabled={cartItems.length === 0} onClick={checkoutHandler}>
+            <div className='d-grid gap-3 mt-2'>
+              <Button type='button' variant='secondary' disabled={cartItems.length === 0} onClick={checkoutHandler}>
                 Proceed To Checkout
               </Button>
               </div>
